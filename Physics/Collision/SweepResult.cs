48f4cb24a6dfb18db240a159c19dc6608abac419
David-Hawkins1 <littlefredhawkins@gmail.com>
2026-09-25 15:48:09 +0100

    Add files via upload

---
+++ Physics/Collision/SweepResult.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGameLibrary.Physics.Collision
+{
+    public struct SweepResult
+    {
+        public bool Hit { get; set; }
+        public float Time { get; set; }
+        public Vector2 Normal { get; set; }
+    }
+}
